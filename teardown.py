9b59192dbb5c177d2853967782a2ae06e8d47ff5
Graham Enos <graham@strangeworks.com>
2025-01-13 11:28:24 -0500

    pathlib ftw

--- teardown.py
+++ teardown.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """Uninstall my dotfiles"""
 
-import os
 from pathlib import Path
 
 CONFIG = Path(__file__).parent.resolve() / Path("config.yaml")
@@ -10,8 +9,9 @@ CONFIG = Path(__file__).parent.resolve() / Path("config.yaml")
 if __name__ == "__main__":
     # Remove all symlinks listed in the CONFIG
     for line in CONFIG.read_text().strip().split("\n"):
+        symlink = Path.home() / line.split(": ")[1]
         try:
-            os.unlink(str(Path.home() / line.split(": ")[1]))
+            symlink.unlink()
         except FileNotFoundError:
             pass
     print("Done!")
