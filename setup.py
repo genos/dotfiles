b5de0b2dff29f8f51d98a53541ab185bd4a8e3e8
Graham Enos <graham@strangeworks.com>
2025-02-26 11:38:52 -0500

    squeeze

--- setup.py
+++ setup.py
@@ -6,14 +6,11 @@ from pathlib import Path
 
 DOTFILES = Path(__file__).parent.resolve()
 CONFIG = DOTFILES / Path("config.yaml")
-
-
-if __name__ == "__main__":
-    # Split a line in the CONFIG and link the right to the left
-    for line in CONFIG.read_text().strip().split("\n"):
-        left, right = line.split(": ")
-        actual, symlink = DOTFILES / left, Path.home() / right
-        if not symlink.parent.exists():
-            os.makedirs(symlink.parent)
-        symlink.symlink_to(actual)
-    print("Done!")
+# Split a line in the CONFIG and link the right to the left
+for line in CONFIG.read_text().strip().split("\n"):
+    left, right = line.split(": ")
+    actual, symlink = DOTFILES / left, Path.home() / right
+    if not symlink.parent.exists():
+        os.makedirs(symlink.parent)
+    symlink.symlink_to(actual)
+print("Done!")
--- teardown.py
+++ teardown.py
@@ -4,14 +4,11 @@
 from pathlib import Path
 
 CONFIG = Path(__file__).parent.resolve() / Path("config.yaml")
-
-
-if __name__ == "__main__":
-    # Remove all symlinks listed in the CONFIG
-    for line in CONFIG.read_text().strip().split("\n"):
-        symlink = Path.home() / line.split(": ")[1]
-        try:
-            symlink.unlink()
-        except FileNotFoundError:
-            pass
-    print("Done!")
+# Remove all symlinks listed in the CONFIG
+for line in CONFIG.read_text().strip().split("\n"):
+    symlink = Path.home() / line.split(": ")[1]
+    try:
+        symlink.unlink()
+    except FileNotFoundError:
+        pass
+print("Done!")
